f757d42e68772f3d6e4e1555105bc2c50e146c73
Ananthdev24796 <ananth.cr7@gmail.com>
2022-09-08 23:56:22 +0530

    Added: Swap two number

---
+++ src/com/bridgelabz/Swap.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.bridgelabz;
+
+/**
+ * @author Iam_A
+ *
+ */
+public class Swap {
+	/**
+	 * Swap two number
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b= 20;
+		int c;
+		System.out.println("value of a before swap:"+a);
+		System.out.println("value of b before swap:"+b);
+		c=a;
+		a=b;
+		b=c;
+		
+		System.out.println("value of a after swap:"+a);
+		System.out.println("value of b after swap:"+b);
+		
+
+	}
+
+}
